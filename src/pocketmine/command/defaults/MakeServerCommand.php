46e648ad112bfdf8b2c66ea0104f08b4e1c44923
Исрапил Ахмедов <CloudMinePE@gmail.com>
2017-04-09 21:20:06 +0300

    Update MakeServerCommand.php

--- src/pocketmine/command/defaults/MakeServerCommand.php
+++ src/pocketmine/command/defaults/MakeServerCommand.php
@@ -22,7 +22,7 @@ class MakeServerCommand extends VanillaCommand{
 		}
 
 		$server = $sender->getServer();
-		$pharPath = Server::getInstance()->getPluginPath() . DIRECTORY_SEPARATOR . "Tesseract" . DIRECTORY_SEPARATOR . $server->getName() . "_" . $server->getPocketMineVersion() . ".phar";
+		$pharPath = Server::getInstance()->getPluginPath() . "Tesseract" . DIRECTORY_SEPARATOR . $server->getName() . "_" . $server->getPocketMineVersion() . ".phar";
 		if(file_exists($pharPath)){
 			$sender->sendMessage("Phar file already exists, overwriting...");
 			@unlink($pharPath);
