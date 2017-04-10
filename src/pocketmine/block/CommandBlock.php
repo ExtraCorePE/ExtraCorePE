92104d70135a40341c1e7eeb4317c7f2384f245b
Nought57 <SkyDrifter2000@gmail.com>
2017-04-11 05:19:46 +0900

    Add CommandBlock
    place only.

---
+++ src/pocketmine/block/CommandBlock.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ *
+ *    ______      _              _____               _____  ______ 
+ *   |  ____|    | |            / ____|             |  __ \|  ____|
+ *   | |__  __  _| |_ _ __ __ _| |     ___  _ __ ___| |__) | |__   
+ *   |  __| \ \/ / __| '__/ _` | |    / _ \| '__/ _ \  ___/|  __|  
+ *   | |____ >  <| |_| | | (_| | |___| (_) | | |  __/ |    | |____ 
+ *   |______/_/\_\\__|_|  \__,_|\_____\___/|_|  \___|_|    |______|
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author ExtraCorePE
+ * @link http://www.github.com/ExtraCorePE/ExtraCorePE
+ * 
+ *
+ */
+
+namespace pocketmine\block;
+
+use pocketmine\item\Item;
+
+class CommandBlock extends Solid{
+
+	protected $id = self::COMMAND_BLOCK;
+
+	public function __construct($meta = 0){
+		$this->meta = $meta;
+	}
+
+	public function getName() : string{
+		return "Command Block";
+	}
+
+	public function getHardness() {
+		return -1;
+	}
+
+	public function getResistance(){
+		return 18000000;
+	}
+
+	public function isBreakable(Item $item){
+		return false;
+	}
+
+}
