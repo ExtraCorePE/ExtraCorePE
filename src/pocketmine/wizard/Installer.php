c9a2963d5b199a0f8be79ce55ae690edf9d99161
Исрапил Ахмедов <CloudMinePE@gmail.com>
2017-04-11 18:03:23 +0300

    Fix config tipe in installer

--- src/pocketmine/wizard/Installer.php
+++ src/pocketmine/wizard/Installer.php
@@ -183,7 +183,7 @@ LICENSE;
 		if($op === ""){
 			echo "[!] " . $this->lang->op_warning . "\n";
 		}else{
-			$ops = new Config(\pocketmine\DATA . "ops.json", Config::ENUM);
+			$ops = new Config(\pocketmine\DATA . "ops.json", Config::JSON);
 			$ops->set($op, true);
 			$ops->save();
 		}
